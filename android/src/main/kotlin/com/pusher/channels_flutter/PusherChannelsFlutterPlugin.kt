09a4b2b0f2f25a5ccf0d07e39f968925e78d9266
Slightly Rudge <58025575+ZhiIsland@users.noreply.github.com>
2025-08-09 13:25:35 +0800

    Update PusherChannelsFlutterPlugin.kt

--- android/src/main/kotlin/com/pusher/channels_flutter/PusherChannelsFlutterPlugin.kt
+++ android/src/main/kotlin/com/pusher/channels_flutter/PusherChannelsFlutterPlugin.kt
@@ -120,10 +120,10 @@ class PusherChannelsFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAw
                 options.proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress(host, port.toInt()))
             }
             if (call.argument<String>("wsPort") != null){
-                options.setWsPort(call.argument("wsPort").toInt())
+                options.setWsPort(call.argument("wsPort"))
             }
             if (call.argument<String>("wssPort") != null) {
-                options.setWssPort(call.argument("wssPort").toInt())
+                options.setWssPort(call.argument("wssPort"))
             }
             if (call.argument<String>("host") != null) {
                 options.setHost(call.argument("host")!!)
